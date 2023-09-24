972c4f1184c8cdc2f4d6a765f57934eafcb43dbc
MSAKabir <shaikhak707@gmail.com>
2023-09-24 08:13:54 +0600

    update

--- web_site/urls.py
+++ web_site/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-
+from . import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include("web.urls"))
 ]
+# if settings.DEBUG:
+#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

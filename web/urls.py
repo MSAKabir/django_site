ef90c430bb2361c86d3002bf4d7a4baf3d83c15b
MSAKabir <shaikhak707@gmail.com>
2023-09-24 09:50:23 +0600

    update

--- web/urls.py
+++ web/urls.py
@@ -3,11 +3,11 @@ from django.urls import include, path
 from . import views
 urlpatterns = [
     path('',views.home,name=""),
-    path('research',views.research,name="research"),
-    path('project',views.project,name="project"),
-    path('skill',views.skill,name="skill"),
-    path('course',views.course,name="course"),
-    path('award',views.award,name="award"),
-    path('switch',views.switch,name="switch"),
+    # path('research',views.research,name="research"),
+    # path('project',views.project,name="project"),
+    # path('skill',views.skill,name="skill"),
+    # path('course',views.course,name="course"),
+    # path('award',views.award,name="award"),
+    # path('switch',views.switch,name="switch"),
     # path('project/Audio8D', views.Audio8D,name='Audio8D'),
 ]
--- web/views.py
+++ web/views.py
@@ -1,19 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 def home(request):
-    return render(request,"web/index.html")
-def research(request):
-    return render(request,"web/research.html")
-def project(request):
-    return render(request,"web/project.html")
-def skill(request):
-    return render(request,"web/skill.html")
-def course(request):
-    return render(request,"web/course.html")
-def award(request):
-    return render(request,"web/award.html")
-def switch(request):
-    return render(request,"web/switch.html")
+    return HttpResponse("hello")
+    # return render(request,"web/index.html")
+# def research(request):
+#     return render(request,"web/research.html")
+# def project(request):
+#     return render(request,"web/project.html")
+# def skill(request):
+#     return render(request,"web/skill.html")
+# def course(request):
+#     return render(request,"web/course.html")
+# def award(request):
+#     return render(request,"web/award.html")
+# def switch(request):
+#     return render(request,"web/switch.html")
 
 #
 # def Audio8D(request):
